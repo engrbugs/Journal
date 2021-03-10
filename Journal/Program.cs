c859fc23b7310285e89d80d2cff948d554001038
engrbugs <engrbugs@gmail.com>
2021-03-09 21:38:16 -0800

    v0.91

--- Journal/Program.cs
+++ Journal/Program.cs
@@ -7,31 +7,38 @@ namespace Journal
 {
     class Program
     {
-        const string VERSION = "b09";
+        const string VERSION = "v0.91";
         const string JOURNAL_PATH = @"C:\Users\engrb\OneDrive\bugs\Journal";
         static void Main(string[] args)
         {
             Console.WriteLine($"Creating Journal {VERSION}");
-
+            Console.WriteLine($"Loading WinWord App");
             Word.Application objWord = new Word.Application();
+            Console.WriteLine($"Making it visible and maximized");
             objWord.Visible = true;
             objWord.WindowState = Word.WdWindowState.wdWindowStateMaximize;
 
+            Console.WriteLine($"Creating a document");
             Word.Document objDoc = objWord.Documents.Add();
 
+            Console.WriteLine($"What is the date for today... hmmm...");
             objWord.Selection.TypeText(strHeader1Text());
-            
+
+            Console.WriteLine($"Typing the header");
             objDoc.Paragraphs[1].set_Style(Word.WdBuiltinStyle.wdStyleHeading1);
             objDoc.Paragraphs[1].Range.Underline = Word.WdUnderline.wdUnderlineSingle;
-            
+
+            Console.WriteLine($"Clear formatting for you to type");
             objWord.Selection.TypeParagraph();
             objWord.Selection.TypeText(Environment.NewLine);
             objDoc.Paragraphs[2].set_Style(Word.WdBuiltinStyle.wdStyleNormal);
 
             try
             {
+                Console.WriteLine($"Making a folder");
                 Directory.CreateDirectory($"{JOURNAL_PATH}\\" +
                     $"{strFolderYear()}\\{strFolderMonth()}");
+                Console.WriteLine($"I'm close... Saving the file for you");
                 objDoc.SaveAs2($"{JOURNAL_PATH}\\{strFolderYear()}\\" +
                     $"{strFolderMonth()}\\{strFilename()}.docx");
             }
@@ -39,7 +46,11 @@ namespace Journal
             {
                 Console.WriteLine("The process failed: {0}", e.ToString());
             }
+            
+            Console.WriteLine($"Focus on WinWord app");
             objWord.Activate();
+
+            Console.WriteLine($"Bye.");
         }
 
         static string strHeader1Text()
